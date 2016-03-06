8883a669fae9e5b492b827078938c1e4481517a6
priscillalok1 <priscillalok@gmail.com>
2016-03-06 02:09:30 -0800

    added dummy functionality to view controllers

--- Mafia/LobbyViewController.swift
+++ Mafia/LobbyViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class LobbyViewController: UIViewController {
+class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var game: Game?
 
     @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var startGameButton: UIButton!
     
-    @IBOutlet var playersCollectionView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if Player.currentPlayer?.isGameCreator == false{
+            startGameButton.hidden = true
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
 
     @IBAction func onStartGameClick(sender: AnyObject) {
+        MafiaClient.instance.changeGameStatus("Active") { (game: Game) -> Void in
+            self.game = game
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerTableViewCell", forIndexPath: indexPath) as! PlayerTableViewCell
+        let player = game?.players[indexPath.row]
+        cell.playerNameLabel.text = player?.name
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if game?.players == nil {
+            return 0
+        } else {
+            return (game?.players.count)!
+        }
         
     }
     
--- Mafia/PlayerProfileViewController.swift
+++ Mafia/PlayerProfileViewController.swift
@@ -12,9 +12,12 @@ import AVFoundation
 class PlayerProfileViewController: UIViewController {
 
 //    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var playerNameTextField: UITextField!
-    @IBOutlet weak var codeLabel: UILabel!
     
+    @IBOutlet weak var playerNameTextField: UITextField!
+    var currPlayer: Player?
+
+    
+    //picture taking for avatar variables
     let captureSession = AVCaptureSession()
     var previewLayer : AVCaptureVideoPreviewLayer?
     // If we find a device we'll store it here for later use
@@ -22,62 +25,73 @@ class PlayerProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        captureSession.sessionPreset = AVCaptureSessionPresetLow
-        let devices = AVCaptureDevice.devices()
-        print(devices)
-        print("hello")
-        
-        // Loop through all the capture devices on this phone
-        for device in devices {
-            // Make sure this particular device supports video
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                // Finally check the position and confirm we've got the back camera
-                if(device.position == AVCaptureDevicePosition.Back) {
-                    captureDevice = device as? AVCaptureDevice
-                }
-            }
-        }
-        if captureDevice != nil {
-            beginSession()
-        }
-    }
 
-    func beginSession() {
-        configureDevice()
-        
-        do {
-            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-        }
-        catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        self.view.layer.addSublayer(previewLayer!)
-        previewLayer?.frame = self.view.layer.frame
-        captureSession.startRunning()
+//        captureSession.sessionPreset = AVCaptureSessionPresetLow
+//        let devices = AVCaptureDevice.devices()
+//        print(devices)
+//        print("hello")
+//        
+//        // Loop through all the capture devices on this phone
+//        for device in devices {
+//            // Make sure this particular device supports video
+//            if (device.hasMediaType(AVMediaTypeVideo)) {
+//                // Finally check the position and confirm we've got the back camera
+//                if(device.position == AVCaptureDevicePosition.Back) {
+//                    captureDevice = device as? AVCaptureDevice
+//                }
+//            }
+//        }
+//        if captureDevice != nil {
+//            beginSession()
+//        }
     }
+//
+//    func beginSession() {
+//        configureDevice()
+//        
+//        do {
+//            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+//        }
+//        catch let error as NSError {
+//            print("error: \(error.localizedDescription)")
+//        }
+//        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+//        self.view.layer.addSublayer(previewLayer!)
+//        previewLayer?.frame = self.view.layer.frame
+//        captureSession.startRunning()
+//    }
+//    
+//    func configureDevice() {
+//        if let device = captureDevice {
+//            do {
+//                try device.lockForConfiguration()
+//            }
+//            catch let error as NSError {
+//                print("error: \(error.localizedDescription)")
+//            }
+//            device.focusMode = .Locked
+//            device.unlockForConfiguration()
+//        }
+//        
+//    }
     
-    func configureDevice() {
-        if let device = captureDevice {
-            do {
-                try device.lockForConfiguration()
-            }
-            catch let error as NSError {
-                print("error: \(error.localizedDescription)")
-            }
-            device.focusMode = .Locked
-            device.unlockForConfiguration()
-        }
-        
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-   
+    
     @IBAction func onNextButtonClick(sender: AnyObject) {
+        MafiaClient.instance.createGame { (game: Game) -> Void in
+            self.currPlayer?.isGameCreator = true
+            
+            self.currPlayer?.name = self.playerNameTextField.text!
+            game.players.append(self.currPlayer!)
+            
+            Game.currentGame = game
+            Player.currentPlayer = self.currPlayer
+        }
     }
 
     /*
